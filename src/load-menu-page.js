2ba5d949bdd4f18a3e012c8bf0a1e7b330ccb60f
Vimpandra <victorimperatriz@gmail.com>
2022-11-29 13:08:09 -0300

    Added logic to generate the menu and html for the menu page

--- src/load-menu-page.js
+++ src/load-menu-page.js
@@ -1,9 +1,59 @@
+let menuItens = [];
+
+function MenuItem(title, desc, price) {
+    this.title = title;
+    this.desc = desc;
+    this.price = price;
+};
+
+const fishNchips = new MenuItem(
+    `Fish 'n Chips`,
+    `Fresh fish of the day, served with boiled potatoes, carrots and herbs.`,
+    `29,90`
+);
+menuItens.push(fishNchips);
+
+const catchicken = new MenuItem(
+    `Catchicken`,
+    `Minced roasted chicken, served with grains and herbs, nice and crunchy.`,
+    `23,90`
+);
+menuItens.push(catchicken);
+
 function loadMenuPage() {
     const content = document.getElementById(`content`);
+
+    const titleContainer = document.createElement(`div`);
+    titleContainer.classList.add(`menuTitle`);
+    content.appendChild(titleContainer);
+
+    const menuTitle = document.createElement(`h1`);
+    menuTitle.textContent = `Menu`;
+    titleContainer.appendChild(menuTitle);
+
+    const menuDesc = document.createElement(`p`);
+    menuDesc.textContent = `Updated daily with fresh tasty options`;
+    titleContainer.appendChild(menuDesc);
     
-    const hi = document.createElement(`h1`);
-    hi.textContent = `Hello, world`;
-    content.appendChild(hi);
+    for (let i = 0; i < menuItens.length; i++) {
+        const itemCard = document.createElement(`div`);
+        itemCard.classList.add(`menuItemCard`);
+        content.appendChild(itemCard);
+
+        const itemTitle = document.createElement(`h3`);
+        itemTitle.textContent = menuItens[i].title;
+        itemCard.appendChild(itemTitle);
+
+        const itemDesc = document.createElement(`p`);
+        itemDesc.classList.add(`itemDesc`);
+        itemDesc.textContent = menuItens[i].desc;
+        itemCard.appendChild(itemDesc);
+
+        const itemPrice = document.createElement(`p`);
+        itemPrice.classList.add(`itemPrice`);
+        itemPrice.textContent = menuItens[i].price;
+        itemCard.appendChild(itemPrice);
+    }
 };
 
 export { loadMenuPage };
